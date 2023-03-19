0caf3c2bc58802ca4e87ff858017d072cae7c619
srohlfing92 <srohlfing@ucsb.edu>
2023-03-19 08:47:17 -0700

    final edits

--- R/package_data.R
+++ R/package_data.R
@@ -1,6 +1,6 @@
 #' Runoff data
 #'
-#' A data fram of the inputs for runoff_rates function including #' fertilizer application rates, land area, and precipitation
+#' A data frame of the inputs for runoff_rates function including fertilizer application rates, land area, and precipitation
 #'
 #' @format a data frame with
 #' \itemize{
@@ -8,7 +8,7 @@
 #' \item land_area
 #' \item precipitation
 #' \item water_quality
-#' \item nutrient concentration
+#' \item nutrient_concentration
 #'}
 "package_data"
 
